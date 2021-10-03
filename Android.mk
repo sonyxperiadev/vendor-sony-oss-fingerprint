sonyxperiadev/vendor-sony-oss-fingerprint

commit 74f45999344adb3b9a2c0c31c521593871e06838

Android.mk: enable fingerprint for edo devices
Enable the Fingerprint for edo device.

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

Android.mk
@@ -43,11 +43,11 @@ LOCAL_CFLAGS += \
 endif
 
 # ---------------- Egistec ----------------
-ifeq ($(filter-out nile ganges kumano seine,$(SOMC_PLATFORM)),)
+ifeq ($(filter-out nile ganges kumano seine edo,$(SOMC_PLATFORM)),)
 LOCAL_CFLAGS += -DFINGERPRINT_TYPE_EGISTEC
 endif
 
-ifeq ($(filter-out kumano seine,$(SOMC_PLATFORM)),)
+ifeq ($(filter-out kumano seine edo,$(SOMC_PLATFORM)),)
 LOCAL_CFLAGS += \
     -DEGISTEC_SAVE_TEMPLATE_RETURNS_SIZE \
     -DEGIS_QSEE_APP_NAME=\"egista\" \
@@ -58,7 +58,7 @@ LOCAL_CFLAGS += \
 endif
 
 # Define dynamic power management for everything but the following platforms:
-ifneq ($(filter-out kumano seine,$(SOMC_PLATFORM)),)
+ifneq ($(filter-out kumano seine edo,$(SOMC_PLATFORM)),)
 LOCAL_CFLAGS += -DHAS_DYNAMIC_POWER_MANAGEMENT
 endif
 

commit 4a15d7e55460d83658304e382978765da9e989ce

android: Allow dynamic power management on Edo
Edo devices seem to have no trouble with dynamically cutting power to
the fingerprint sensor when it's not used, which is especially relevant
considering the lack of gesture navigation.  Without this feature the
sensor can be fully turned off while the user is operating the device
(ie. it's unlocked).
Actual drain is probably negligible but it's reasonable to represent
what the hardware supports nevertheless.  It appears Edo was excluded
from this feature during enablement for no good reason other than Kumano
and Seine also being in this list.

Fixes: 74f4599 ("Android.mk: enable fingerprint for edo devices")

Android.mk
@@ -58,7 +58,7 @@ LOCAL_CFLAGS += \
 endif
 
 # Define dynamic power management for everything but the following platforms:
-ifneq ($(filter-out kumano seine edo,$(SOMC_PLATFORM)),)
+ifneq ($(filter-out kumano seine,$(SOMC_PLATFORM)),)
 LOCAL_CFLAGS += -DHAS_DYNAMIC_POWER_MANAGEMENT
 endif
 