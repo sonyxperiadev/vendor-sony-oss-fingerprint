326173188549dffd9598b9c23c7dba102cfdcfb7
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2022-07-25 10:32:45 +0200

    Android.mk: Add Nile to platforms with Egistec
    In previous commit ifeq was changed to ifneq and devices
    that do not have egistec fp were added into this list.
    
    Nile has Egistec and also can have different fpc.
    
    Thus leave remove it from the list of platforms that do not have Egistec.
    
    Fixes: 694d5f6e2655a8dfbccb909897d27f5369229274
    
    Signed-off-by: Martin Botka <martin.botka@somainline.org>

--- Android.mk
+++ Android.mk
@@ -43,7 +43,7 @@ LOCAL_CFLAGS += \
 endif
 
 # ---------------- Egistec ----------------
-ifneq ($(filter-out loire tone yoshino nile tama,$(SOMC_PLATFORM)),)
+ifneq ($(filter-out loire tone yoshino tama,$(SOMC_PLATFORM)),)
 LOCAL_CFLAGS += -DFINGERPRINT_TYPE_EGISTEC
 endif
 
